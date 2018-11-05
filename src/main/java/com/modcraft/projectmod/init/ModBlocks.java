1340103269c146893a9a32ad9adbac689fa46099
Daniel Crespo <daniel.crespo1@upr.edu>
2018-11-05 15:00:07 -0400

    correctly fixed lasha sand block properties

--- src/main/java/com/modcraft/projectmod/init/ModBlocks.java
+++ src/main/java/com/modcraft/projectmod/init/ModBlocks.java
@@ -3,12 +3,14 @@ package com.modcraft.projectmod.init;
 import java.util.ArrayList;
 import java.util.List;
 import com.modcraft.projectmod.blocks.BlockBase;
+import com.modcraft.projectmod.blocks.LashaSandBlock;
+
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 
 public class ModBlocks {
 	
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
-	public static final Block LASHA_SAND_BLOCK = new BlockBase("lasha_sand_block", Material.ROCK);
+	public static final Block LASHA_SAND_BLOCK = new LashaSandBlock("lasha_sand_block", Material.ROCK);
 
 }
