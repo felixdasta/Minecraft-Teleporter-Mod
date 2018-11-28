add66870995755ecd0df8d44846d6ee03de798a7
Daniel Crespo <daniel.crespo1@upr.edu>
2018-11-28 18:18:33 -0400

    added dragon eye and started config file

--- src/main/java/com/modcraft/projectmod/proxy/ClientProxy.java
+++ src/main/java/com/modcraft/projectmod/proxy/ClientProxy.java
@@ -1,5 +1,7 @@
 package com.modcraft.projectmod.proxy;
 
+import com.modcraft.projectmod.config.ModConfig;
+
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.item.Item;
 import net.minecraftforge.client.model.ModelLoader;
@@ -9,6 +11,10 @@ public class ClientProxy extends CommonProxy {
 	public void registerItemRederer(Item item, int meta, String id) {
 		
 		ModelLoader.setCustomModelResourceLocation(item, meta, new ModelResourceLocation(item.getRegistryName(), id));
+	}
+	
+	public void preInit() {
+		ModConfig.clientPreInit();
 	}
 
 }
